820b3a04797a79673f1adfd68d9600a636708ca1
Lucas Lima <lucas.souza3146@gmail.com>
2023-07-22 20:15:18 -0300

    Exportando e Importando um Componente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import { Square, Circle } from '@/components/Geo';
+
 const Page = () => {
   return (
     <div>
       <h1 className="font-bold text-2xl">Olá Mundo</h1>
       <h3>Alguma outra coisa</h3>
+
+      <Square />
+      <Circle />
     </div>
   );
 }
---
+++ src/components/Geo.tsx
@@ -0,0 +1,15 @@
+export const Square = () => {
+    return (
+        <div className="w-52 h-52 bg-orange-700 text-white">
+            Texto qualquer inspirador
+        </div>
+    )
+}
+
+export const Circle = () => {
+    return (
+        <div className="w-52 h-52 bg-blue-700 text-white rounded-full">
+            Texto qualquer inspirador
+        </div>
+    )
+}
