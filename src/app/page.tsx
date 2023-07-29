b7d1b3e275b139a887ac64a8bca3ed7596624709
Lucas Lima <lucas.souza3146@gmail.com>
2023-07-29 13:35:03 -0300

    Operador lógico &&

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Card } from '@/components/Card';
 import { GeoForm } from '@/components/GeoForm';
 import { Person } from '@/components/Person';
 
@@ -7,14 +8,10 @@ const Page = () => {
       <h1 className="font-bold text-2xl">Olá Mundo</h1>
       <h3>Alguma outra coisa</h3>
 
-      <Person
-        name="Elon Musk"
-        avatar="https://media.vanityfair.com/photos/609d51288fee0e2d204a86ea/master/w_2560%2Cc_limit/Elon5.13.jpg"
-        roles={["CEO da Tesla", "CEO da SpaceX", "CEO da ..."]}/>
-      <Person
-        name="Jeff Bezes"
-        avatar="https://th.bing.com/th/id/OIP.B7wUiEFDbWthzPn5nTGpvAHaE8?w=280&h=187&c=7&r=0&o=5&pid=1.7"
-        roles={["CEO da Amazon", "CEO da Blue Origin"]}/>
+      <Card 
+        phrase="Com grandes poderes vem grandes responsabilidades"
+        
+      />
     </div>
   );
 }
