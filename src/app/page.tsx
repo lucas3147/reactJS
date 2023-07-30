1151a4bc192d02c952c9b9198e436173ab719b80
Lucas Lima <lucas.souza3146@gmail.com>
2023-07-30 15:00:55 -0300

    Greeting condicional 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,11 @@ const Page = () => {
     justify-center items-center text-white bg-gradient-to-r 
     from-sky-500 to-indigo-500">
       <div className="text-9xl">{fullTime}</div>
-      <div className="text-5xl font-bold">{greeting}</div>
+      <div className="text-5xl font-bold">
+        {hour >= 0 && hour < 12 && 'Bom dia 😊'}
+        {hour >= 12 && hour < 18 && 'Boa tarde 😎'}
+        {hour >= 18 && hour < 23 && 'Boa noite 😴'}
+      </div>
     </div>
   );
 }
