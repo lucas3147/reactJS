09b60a4563730267080beeb00bc5d64c6a6fbe69
Lucas Lima <lucas.souza3146@gmail.com>
2023-07-30 14:34:38 -0300

    Filtrando listas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,16 @@ import { peopleList } from '@/data/peopleList';
 
 const Page = () => {
 
+  const chemists = peopleList.filter(person => person.profession === 'chemist');
+
   return (
     <div>
       <h1 className="font-bold text-2xl">Olá Mundo</h1>
       <h3>Alguma outra coisa</h3>
 
-      {peopleList.length > 0 && 
+      {chemists.length > 0 && 
         <ul>
-        {peopleList.map(person => 
+        {chemists.map(person => 
           <li key={person.id}>{person.name} - {person.profession}</li>
         )}
         </ul>
