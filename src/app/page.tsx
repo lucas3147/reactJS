6ab7c1fbebb74563ec5b62d12db46aefc3c124ef
Lucas Lima <lucas.souza3146@gmail.com>
2023-08-05 10:46:39 -0300

    O que é um State?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,16 @@
 "use client"
 
-import { FormEvent } from "react";
-
 const Page = () => {
+  let count = 0;
 
-  const handleFormSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    alert("Opa, mandando...");
+  const handleClickButton = () => {
+    count++;
   }
 
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <h1 className="text-5xl mb-3">Form de Login</h1>
-      <form onSubmit={handleFormSubmit}>
-        <input type="text" />
-        <input type="submit" value="Enviar" />
-      </form>
+      <p>{count}</p>
+      <button onClick={handleClickButton} className="bg-blue-500 p-3">+1</button>
     </div>
   );
 }
