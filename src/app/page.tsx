4e2b5cb2cd3c10ed95c2839a120f25d99a2db0d2
Lucas Lima <lucas.souza3146@gmail.com>
2023-08-05 13:16:34 -0300

    Usando states para auxiliar UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,21 @@
 import { useState } from "react";
 
 const Page = () => {
-  const [count, setCount] = useState<number>(0);
+
+  const [showSecret, setShowSecret] = useState(false);
 
   const handleClickButton = () => {
-    setCount(count + 1);
+    setShowSecret(!showSecret);
   }
 
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <p>{count}</p>
-      <button onClick={handleClickButton} className="bg-blue-500 p-3">+1</button>
+      <button onClick={handleClickButton} className="bg-blue-500 p-3">
+        {showSecret ? 'Ocultar': 'Mostrar'} área secreta
+      </button>
+      {showSecret && 
+        <div className="p-3 bg-blue-300 rounded-md mt-3">Área Secreta</div>
+      }
     </div>
   );
 }
