08a69178ec3f619afb27ca82b7d1de796159121c
Lucas Lima <lucas.souza3146@gmail.com>
2023-08-05 10:03:22 -0300

    Adicionando evento de clique

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,16 @@
+"use client"
+
 const Page = () => {
 
-  const fullTime = new Intl.DateTimeFormat('pt-BR', {
-    timeStyle: 'short',
-    hour12: false
-  }).format();
-
-  const hour = new Date().getHours();
-  let greeting;
-
-  if (hour >= 0 && hour < 12) {
-    greeting = 'Bom dia 😊';
-  } else if (hour >= 12 && hour < 18) {
-    greeting = 'Boa tarde 😎';
-  } else if (hour >= 18 && hour <= 23) {
-    greeting = 'Boa noite 😴'
+  function handleClick() {
+    alert('Funcionou!');
   }
 
   return (
-    <div className="w-screen h-screen flex flex-col 
-    justify-center items-center text-white bg-gradient-to-r 
-    from-sky-500 to-indigo-500">
-      <div className="text-9xl">{fullTime}</div>
-      <div className="text-5xl font-bold">
-        {hour >= 0 && hour < 12 && 'Bom dia 😊'}
-        {hour >= 12 && hour < 18 && 'Boa tarde 😎'}
-        {hour >= 18 && hour < 23 && 'Boa noite 😴'}
-      </div>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <button onClick={handleClick} className="p-3 bg-blue-700 text-white rounded-md">
+        Clique aqui
+      </button>
     </div>
   );
 }
