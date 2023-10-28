563c41998d9d6de4dd4c2b0c32cb0d1e631b299a
Lucas Lima <lucas.souza3146@gmail.com>
2023-10-28 12:12:42 -0300

    Fazendo uma query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,37 @@
 "use client"
 
-export default function Home() {
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { Post } from "../types/Post";
+
+const Page = () => {
+
+  const query = useQuery({
+    queryKey: ['posts'],
+    queryFn: async (): Promise<Post[]> =>{
+      const result = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      return result.data;
+    }
+  });
+
   return (
     <div className="container mx-auto">
       <h1 className="text-white text-3xl">Opa, tudo bem?</h1>
+
+      {query.isLoading && 
+        <p>Carregando...</p>
+      }
+
+
+      {query.data &&
+        <ul>
+        {query.data.map(item => 
+            <li key={item.id}>{item.title}</li>
+          )}
+        </ul>
+      }
     </div>
   )
 }
+
+export default Page;
