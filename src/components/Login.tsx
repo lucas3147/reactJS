295b07c085d5c31d6437b15565ec7c98f99a0e95
Lucas Lima <lucas.souza3146@gmail.com>
2023-12-12 22:14:18 -0300

    Adicionando um formulário para confirmação dos dados

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,16 +1,23 @@
 import Api from "@/Api";
 import { UserType } from "@/types/UserType";
+import { useState } from "react";
 
 type Props = {
     onReceive: (userRegister: UserType) => Promise<void>
 }
 
 const Login = ({onReceive}: Props) => {
+    const [userRegister, setUserRegister] = useState<UserType>();
     const handleLogin = async () => {
+        if (userRegister) {
+            onReceive(userRegister);
+        }
+    }
+
+    const handleSignUp = async () => {
         let user = await Api.githubPopup();
         if (user){
-            const userRegister: UserType = {id: user.uid, displayName: user.displayName, photoURL: user.photoURL}
-            onReceive(userRegister);
+            setUserRegister({id: user.uid, displayName: user.displayName, photoURL: user.photoURL});
         } else {
             alert('Não foi possível logar')
         }
@@ -18,7 +25,17 @@ const Login = ({onReceive}: Props) => {
 
     return (
         <div className="flex justify-center items-center h-screen">
-            <button onClick={handleLogin} className="rounded-md px-4 py-2 border-[1px] border-black text-white bg-[#161A1F]">Logar com o github</button>
+            {userRegister == null && 
+                <button 
+                    onClick={handleSignUp} 
+                    className="rounded-md px-4 py-2 border-[1px] border-black text-white bg-[#161A1F]"
+                >
+                    Logar com o github
+                </button>
+            }
+            {userRegister && 
+                <div>oi...</div>
+            }
         </div>
     )
 }
