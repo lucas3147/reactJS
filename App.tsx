b885129482e7b9cd639e31681f413f5d97817d7d
lucas3146 <lucas.souza3146@gmail.com>
2024-01-06 15:03:10 -0300

    Introdução ao React Native

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={
+        {color:'white'}
+      }>Meu primeiro app no celular!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
