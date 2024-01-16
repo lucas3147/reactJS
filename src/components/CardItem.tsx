2d62a52b86c3142333220006bb60abbaa6ffd801
Lucas <lucas.souza3146@gmail.com>
2024-01-16 09:48:32 -0300

    Interface atualizada

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -1,10 +1,10 @@
 const CardItem = ({title, subtitle, onSubmit} : {title: string, subtitle: string, onSubmit: () => void}) => {
     return (
         <div className="w-full px-6 mb-2">
-            <div className="rounded-md bg-zinc-600 border-b-2 border-b-zinc-500 flex items-center justify-between py-4 px-6">
+            <div className="rounded-md bg-zinc-600 border-b-4 border-b-zinc-500 hover:border-b-zinc-400 hover:bg-zinc-500 flex items-center justify-between py-4 px-6">
             <div className="flex items-center">
                 <img 
-                    src='https://cdn.pixabay.com/photo/2015/07/19/10/00/school-work-851328_1280.jpg'
+                    src='https://th.bing.com/th/id/OIP.kbTriMj81wYGYzvVRmWvTAHaE8?rs=1&pid=ImgDetMain'
                     alt="imagem de estudo"
                     className="w-28 h-28 rounded-full mr-6" />
                 <div className="mr-6">
@@ -14,7 +14,7 @@ const CardItem = ({title, subtitle, onSubmit} : {title: string, subtitle: string
             </div>
             
             <button 
-                className="px-6 py-4 bg- rounded-md bg-zinc-800 hover:border-2"
+                className="px-6 py-4 bg- rounded-md bg-zinc-800 border-b-4 border-b-zinc-500 hover:border-b-white"
                 onClick={onSubmit}
                 >
                 acessar
