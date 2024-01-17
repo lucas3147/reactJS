04b277541f265b076e968926359b8f4d1a375d76
Lucas <lucas.souza3146@gmail.com>
2024-01-17 14:30:00 -0300

    Alterando página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,30 @@
 'use client'
 import CardItem from "@/components/CardItem";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { ChangeEvent, ChangeEventHandler, EventHandler, useEffect, useState } from "react";
 import { CardItemType } from "@/types/CardItemType";
 
 
 export default function Home() {
   const router = useRouter();
 
-  const [myExamples] = useState<CardItemType[]>([
-      {title: 'cards simples', subtitle: 'Padrão simples para utilizar', onSubmit: () => router.push('/SlideCards')},
-      {title: 'Exibição em mini tela', subtitle: 'Exibir página sem abrir', onSubmit: () => router.push('/viewerMinimized')},
-      {title: 'Gravador de tela', subtitle: 'Gravar a janela aberta da página', onSubmit: () => router.push('/screenRecorder')}
-    ]);
+  const ListExamples = [
+    {title: 'cards simples', subtitle: 'Padrão simples para utilizar', onSubmit: () => router.push('/SlideCards')},
+    {title: 'Exibição em mini tela', subtitle: 'Exibir página sem abrir', onSubmit: () => router.push('/viewerMinimized')},
+    {title: 'Gravador de tela', subtitle: 'Gravar a janela aberta da página', onSubmit: () => router.push('/screenRecorder')}
+  ]
+
+  const [myExamples, setMyExamples] = useState<CardItemType[]>(ListExamples);
+  const [searchExample, setSearchExample] = useState('');
+
+  const handleSearchExample = (e: any) => {
+    setSearchExample(e.target.value);
+
+    console.log(e.target.value);
+    let examples = ListExamples.filter(ex => ex.title.includes(e.target.value) || e.target.value.trim() == '');
+
+    setMyExamples(examples);
+  }
 
   return (
     <main
@@ -23,14 +35,11 @@ export default function Home() {
       >
         <div className="w-full flex flex-row py-4 px-6">
           <input type="text"
-            className="flex-1 h-10 rounded-md bg-zinc-800 mr-2 px-4 py-6 outline-none"
+            className="w-full h-10 rounded-[15px] bg-zinc-800 px-4 py-6 outline-none"
             placeholder="Pesquisar exemplo..."
+            value={searchExample}
+            onChange={handleSearchExample}
           />
-          <button
-            className="rounded-md bg-zinc-800 px-4 hover:border-2"
-          >
-            Pesquisar
-          </button>
         </div>
 
         <div
