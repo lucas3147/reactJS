c799897a0f7876e0d91e6d94ddb6134428f2a974
Lucas <lucas.souza3146@gmail.com>
2024-01-17 13:49:41 -0300

    Inserindo conteúdo na tela para testarmos as gravações

--- src/app/(examples)/screenRecorder/page.tsx
+++ src/app/(examples)/screenRecorder/page.tsx
@@ -1,18 +1,53 @@
+'use client'
 import CardTitle from "@/components/CardTitle"
 import ContainerPages from "@/components/Container"
+import { useState } from "react";
 
 const screenRecorder = () => {
+    const [tab1, setTab1] = useState(true);
+    const [tab1Value, setTab1Value] = useState('Teste gravação 01')
+    const [tab2Value, setTab2Value] = useState('Teste gravação 02')
+
     return (
         <ContainerPages>
             <CardTitle
                 title='Gravador de tela'
                 subtitle='Para gravar a janela aberta no lado do cliente e exportar esse vídeo. Template pronto para utilizar'
             />
-            <button
-                className="bg-zinc-700 px-10 py-6 font-bold uppercase rounded-lg border-b-8 border-b-zinc-400"
-            >
-                Gravar
-            </button>
+            <div className="flex items-center">
+                <button
+                    className="bg-zinc-700 px-10 py-6 font-bold uppercase rounded-lg border-b-8 border-b-zinc-400 mr-40"
+                >
+                    Gravar
+                </button>
+                <div>
+                    <div className="flex">
+                        <button 
+                            className="p-4 bg-zinc-700 border-r-black border-r-2 rounded-t-md hover:bg-zinc-800"
+                            onClick={() => setTab1(true)}
+                        >
+                            Tab 01
+                        </button>
+                        <button 
+                            className="p-4 bg-zinc-700 rounded-t-md hover:bg-zinc-800"
+                            onClick={() => setTab1(false)}
+                        >
+                            Tab 02
+                        </button>
+                    </div>
+                    <div className="w-96 h-80 bg-zinc-400 rounded-b-md rounded-tr-md p-2">
+                        {tab1 &&
+                            <textarea value={tab1Value} onChange={(e) => setTab1Value(e.target.value)} className="w-full h-full text-black rounded-md p-2 font-bold text-2xl">
+                            </textarea>
+                        }
+                        {!tab1 &&
+                            <textarea value={tab2Value} onChange={(e) => setTab2Value(e.target.value)} className="w-full h-full text-black rounded-md p-2  font-bold  text-2xl">
+                            </textarea>
+                        }
+                    </div>
+                </div>
+            </div>
+            
         </ContainerPages>
     )
 }
